397475a631023657f703dd6abd6b9cbf44406d27
Mohd. Ahmad <ahmadseemapuri@gmail.com>
2022-11-03 00:40:53 +0530

    HW Class 9

--- Class9.java
+++ Class9.java
@@ -7,6 +7,9 @@ public class Class9 {
         // print3(num2);
         // print4(5);
         System.out.println(fact(5));
+        System.out.println(HomeworkOfClass9.pow(5, 4));
+        HomeworkOfClass9.countZeroes(1009080000, 0);
+        HomeworkOfClass9.sumOfDigit(1009082345, 0);
         
     }
     public static int add(int x,int y) {
@@ -61,5 +64,34 @@ public class Class9 {
             return n;
         }
         return n*fact(n-1);
+    }
+}
+class HomeworkOfClass9{
+    public static int pow(int num,int pow) {
+        if(pow==0){
+            return 1; 
+        }
+        if(pow==1){
+            return num;
+        }
+        return num * pow(num, pow-1);
+    }
+    public static void countZeroes(int num,int count) {
+        if(num<=0){
+            System.out.println(count);
+            return;
+        }
+        if (num%10 == 0) {
+            count++;
+        }
+        countZeroes(num/10, count);
+        
+    }
+    public static void sumOfDigit(int num,int sum) {
+        if(num<=0){
+            System.out.println(sum);
+            return;
+        }
+        sumOfDigit(num/10, sum+=num%10);
     }
 }
