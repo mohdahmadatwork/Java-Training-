66f84a1da7345c9a899b40d88dc2d2f90d30bdf6
Mohd. Ahmad <ahmadseemapuri@gmail.com>
2022-11-10 22:32:46 +0530

    HW Class 11

--- Class11.java
+++ Class11.java
@@ -55,12 +55,29 @@ public class Class11 {
         }
         return isPrime(num, sqrtofnum-1);
     }
+    // Prime Sieve Method
+    public static boolean isPrime2(int num,int sqrtofnum,int copy) {
+        if ((num%2==0 || num%3==0) && num>3 ) {
+            return false;
+        }
+        if (sqrtofnum>=copy) {
+            return true;
+        }
+        if (num % sqrtofnum ==0 || num%(sqrtofnum+2) == 0) {
+            return false;
+        }
+        return isPrime(num, sqrtofnum+6);
+
+    }
     public static void print_prime(int num) {
         if(num<=1){
             return;
         }
         print_prime(num-1);
-        if (isPrime(num, (int)Math.sqrt(num))) {
+        // if (isPrime(num, (int)Math.sqrt(num))) {
+        //     System.out.print(num+" ");
+        // }
+        if (isPrime2(num,2,(int)Math.sqrt(num))) {
             System.out.print(num+" ");
         }
     }
@@ -79,8 +96,54 @@ public class Class11 {
         System.out.println();
         print_prime(100);
         System.out.println();
+        HomeworkOfClass11.pattern1(5);
+        HomeworkOfClass11.pattern2(5, 1, 65);
     }
 
 
 }
 
+class HomeworkOfClass11{
+    // Pattern 1
+    // a
+    // ab
+    // abc
+    // abcd
+    // abcde
+    public static void print_row_p1(int n) {
+        if(n<97){
+            return;
+        }
+        print_row_p1(n-1);
+        System.out.print((char)n);
+    }
+    public static void pattern1(int n) {
+        if(n<0){
+            return;
+        }
+        pattern1(n-1);
+        print_row_p1(97+n);
+        System.out.println();
+    }
+    // pattern2
+    // A 
+    // BC 
+    // DEF 
+    // GHIJ
+    // KLMNO
+    public static int print_row_p2(int row,int i) {
+        if(row<=0){
+            return i;
+        }
+        System.out.print((char)i+" ");
+        return print_row_p2(row-1,i+1);
+    }
+    public static void pattern2(int row,int current_row,int i) {
+        if(row<current_row){
+            return;
+        }
+        i=print_row_p2(current_row,i);
+        System.out.println();
+        pattern2(row, current_row+1, i);
+    }
+}
