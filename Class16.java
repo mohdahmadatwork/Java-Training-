f90ee234a0c3eb74dc7daf608504ae31bf6b8bcc
Mohd. Ahmad <ahmadseemapuri@gmail.com>
2022-11-30 20:39:17 +0530

    LeetCode Nqueen Problem

--- Class16.java
+++ Class16.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+import java.util.List;
 public class Class16 {
     public static void main(String[] args) {
         System.out.println(countNumberOfWays(0));
@@ -36,5 +38,69 @@ public class Class16 {
         }
         return true;
 
+    }
+}
+class HomeworkOfClass16{
+    public static void solveNQueens(int n) {
+        ArrayList<String> board = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            String temp = "";
+            for (int j = 0; j < n; j++) {
+                temp+=".";
+            }
+            board.add(temp);
+        }
+        // System.out.println(board);
+        List<List<String>> finalResult = nQueens(board, 0, n);
+        System.out.println(finalResult);
+    }
+
+    public static List<List<String>> nQueens(ArrayList<String> board,int row,int n) {
+        List<List<String>> finalResult = new ArrayList<>();
+        if(row==n){
+            ArrayList<String> ArrList2= (ArrayList<String>)board.clone();
+            finalResult.add(ArrList2);
+            return finalResult;
+        }
+        for (int i = 0; i < n; i++) {
+            if(isSafe(board, row, i,n)){
+                String temp = board.get(row).substring(0,i)+"Q"+board.get(row).substring(i+1);
+                board.set(row,temp);
+                List<List<String>> finalResult1= nQueens(board, row+1,n);
+                for (int j = 0; j < finalResult1.size(); j++) {
+                    finalResult.add(finalResult1.get(i));
+                }
+                board.set(row,board.get(row).substring(0,i)+"."+board.get(row).substring(i+1));
+                // board[row][i] = 'X';
+            }
+        }
+        return finalResult;
+    }
+    public static boolean isSafe(ArrayList<String> board,int row,int col,int n) {
+        // Vertical up
+        for (int i=row-1; i >=0; i--) {
+            // [i][col]
+            if (board.get(i).charAt(col)=='Q') {
+                return false;
+            }
+        }
+        // right diagonal
+        for (int i=row-1, j = col+1; i >=0 && j <n; i--,j++) {
+            // for (; ; ) {
+                // [i][j]
+            if (board.get(i).charAt(j)=='Q') {
+                return false;
+            }
+        }
+        // left Diagonal
+        for (int i=row-1,j = col-1; i >=0 && j >= 0; i--,j--) {
+            // [i][j]
+            if (board.get(i).charAt(j)=='Q') {
+                return false;
+            }
+        }
+
+        return true;
+        
     }
 }
