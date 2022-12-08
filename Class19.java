85d75e26c0524630a8b27f484deb54f3940b8396
Mohd. Ahmad <97238445+ahmad9652@users.noreply.github.com>
2022-12-08 10:14:30 +0530

    LeetCode 39 Combination sum

--- Class19.java
+++ Class19.java
@@ -38,4 +38,44 @@ class MazeCount{
         return count(row+1, col, endRow, endcol)+count(row, col+1, endRow, endcol);
     }
 }
+class HomeworkOfClass19{
+      //Combination sum LeetCode 39
+        public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        List<List<Integer>> realoutput = new ArrayList<>();
+        backTrackSol(candidates,target,0,"",0);
+        for(int i=0;i<output.size();i++){
+            realoutput.add(output.get(i));
+        }
+        output.clear();
+        return realoutput;
+    }
+    static List<List<Integer>> output= new ArrayList<>();
+public static void backTrackSol(int arr[],int element,int index,String reqArr,int sum) {
+        if(sum>element){
+            return;
+        }
+        if (element==sum) {
+            // System.out.println(reqArr+" ")
+            ArrayList<Integer> newArr=new ArrayList<>();
+            String newreqArr [] =  reqArr.split(" ");
+            // for (int i = 0; i < newreqArr.length; i++) {
+            //     System.out.print(newreqArr[i]+" ");
+            // }
+            // System.out.println();
+            for(int i=0;i<newreqArr.length;i++){
+                int index1=Integer.parseInt(newreqArr[i], 10);
+                newArr.add(arr[index1]);
+            }
+            output.add(newArr);
+            return;
+        }
+        if (index>=arr.length) {
+            return;
+        }
+        backTrackSol(arr, element, index, reqArr+index+" ", sum+arr[index]);
+        //backTrackSol(arr,element,index+1,reqArr+index,sum+arr[index]);
+        //backTrackSol(arr, element,index,reqArr,sum);
+       backTrackSol(arr, element, index+1, reqArr, sum);
+    }
+}
 
