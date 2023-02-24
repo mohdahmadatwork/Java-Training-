d70cf436c256ed1a9e7e29cbca75ee201ed37bfd
Mohd. Ahmad <ahmadseemapuri@gmail.com>
2023-02-24 08:29:58 +0530

    HW Class 233

--- Class23.java
+++ Class23.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.HashMap;
 
 public class Class23 {
@@ -11,7 +12,13 @@ public class Class23 {
         pairSum_approach1(arr2, 11);
         pairSum_approach2(arr2, 11);
          
-        
+        boolean isArraySorted = HomeworkOfClass23.isArraySorted(arr2);
+        System.out.print("is given array "+" sorted : "+ isArraySorted);
+        int [] arr3 = {0,0,1,1,1,2,2,3,3,4};
+        printArr(arr3);
+        HomeworkOfClass23.removeDuplicatesApproach1(arr3);
+        printArr(arr3);
+        HomeworkOfClass23.findLeaderApproach1(arr3);
     }
     public static void printArr(int []arr){
         for (int i = 0; i < arr.length; i++) {
@@ -52,4 +59,52 @@ public class Class23 {
         
     }
 }
-
+class HomeworkOfClass23{
+    public static boolean isArraySorted(int[]arr) {
+        for (int i = 0; i < arr.length-1; i++) {
+            if (arr[i]>arr[i+1]) {
+                return false;
+            }
+        }
+        return true;
+    }
+    public static boolean searchInList(ArrayList<Integer> list,int k){
+        for (int j = 0; j < list.size(); j++) {
+            if (list.get(j)==k) {
+                return false;
+            }
+        }
+        return true;
+    }
+    public static int removeDuplicatesApproach1(int[] nums) {
+        ArrayList<Integer> list = new ArrayList<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (searchInList(list, nums[i])) {
+                list.add(nums[i]);
+            }
+        }
+        for (int i = 0; i < list.size(); i++) {
+            nums[i] = list.get(i);
+        }
+        return list.size();
+    }
+    public static boolean isLeader(int [] arr,int k) {
+        for (int j = k+1; j < arr.length; j++) {
+            if(arr[j]>arr[k]){
+                return false;
+            }
+        }
+        return true;
+    }
+    public static void findLeaderApproach1(int [] arr){
+        ArrayList<Integer> list = new ArrayList<>();
+        for (int i = 0; i < arr.length; i++) {
+            if (isLeader(arr, i)) {
+                if (searchInList(list, arr[i])) {
+                    list.add(arr[i]);
+                }
+            }
+        }
+        System.out.println(list);
+    }
+}
