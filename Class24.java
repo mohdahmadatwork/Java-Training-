e2889f9f1c9d49f9738bf6f8ac3fb78396854e1a
Mohd. Ahmad <ahmadseemapuri@gmail.com>
2023-02-28 21:53:48 +0530

    HW Class 24

--- Class24.java
+++ Class24.java
@@ -50,5 +50,85 @@ public class Class24 {
         printArr(arr);
         int arr2[] = {9,5,6,7,8,9,1};
         printArr(arrayAddition(arr, arr2));
+        
+        // Problem 10 
+        int first[] = {9,5,1,7,5,3};
+        int second[] = {9,5,6,7,8,9,1};
+        HomeworkOfClass24.subtracionArr(first, second);
+
+        // Problem 8
+        int [] nums = {1,2,3,4,5,6,7};
+        HomeworkOfClass24.RotateArrayByk_approach_1(nums, nums.length, 3);
+        printArr(nums);
+        // in approach 2 k is the last index means from 0 to ehich index do you want to rotate? while in approach 1 k is the number of element means how many element do you want to rotate
+        HomeworkOfClass24.RotateArrayByk_approach_2(nums, nums.length, 3);
+        printArr(nums);
+
+
     }    
+}
+class HomeworkOfClass24{
+    public static void subtracionArr(int[]first,int[]second) {
+        int [] result = new int[first.length>second.length?first.length:second.length];
+        int i = first.length-1;
+        int j = second.length-1;
+        int k = result.length-1;
+        // int borrow = 0;
+        int diff = 0;
+        while (k>=0) {
+            if (i>j ) {
+                if (i>=0 &&j>=0) {
+                    if (first[i]>=second[j]) {
+                        diff = first[i]-second[j];
+                    }
+                    else{
+                        diff = 10 + first[i] - second [j];
+                        first[i-1]--;
+                    }
+                }
+                else{
+                    diff = first[i];
+                }
+            }
+            else{
+
+                if (i>=0 &&j>=0) {
+                    if (second[j]>=first[i]) {
+                        diff = second[j]-first[i];
+                    }
+                    else{
+
+                        diff = 10 + second [j] - first[i];
+                        second[j-1]--;
+                    }
+                }
+                else{
+                    diff = second[j];
+                }
+            }
+            result[k] = diff;
+            i--;
+            j--;
+            k--;
+        }
+        Class24.printArr(result);
+    }
+    public static void reversePartArr(int[]arr,int start,int end) {
+        for (int i = start , j = end ; i <=(end-start+1)/2 && j>=(end-start+1)/2; i++,j--) {
+            int temp=arr[i];
+            arr[i]=arr[j];
+            arr[j]=temp;
+        }
+    }
+    public static void RotateArrayByk_approach_1 (int[]arr,int n,int k) {
+        for (int i = 0; i < k; i++) {
+            Class24.rotate(arr, n);
+        }
+    }
+    // Optimize approach
+    public static void RotateArrayByk_approach_2(int []arr,int n,int k) {
+        reversePartArr(arr, 0, n-k-1);
+        reversePartArr(arr,  n-k , n-1);
+        reversePartArr(arr, 0, n-1);   
+    }
 }
